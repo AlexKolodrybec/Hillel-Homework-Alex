f70540428028f5f94fedabf2fbbe87fcaa1469a2
Alex Kolodrybec <alex.kolodrybec@gmail.com>
2024-05-06 16:16:51 +0300

    Homework

--- Front-End-Pro/practice_6/test.js
+++ Front-End-Pro/practice_6/test.js
@@ -1,10 +1,18 @@
-let numbers = [1, 2, 3, 4, 5];
+function Calc(num1, num2) {
+    this.num1 = num1;
+    this.num2 = num2;
+    
+    this.getPlus = function() {
+        return this.num1 + this.num2;
+    };
 
-function addToArr(arr, num) {
-    arr.push(num);
+    this.getMinus = function() {
+        return this.num1 - this.num2;
+    };
 }
 
-let newNumber = 6;
-addToArr(numbers, newNumber);
+let calc1 = new Calc(7, 2);
+let calc2 = new Calc(5, 7);
 
-console.log(numbers);
+console.log("Додавання:", calc1.getPlus());
+console.log("Віднімання", calc2.getMinus());
