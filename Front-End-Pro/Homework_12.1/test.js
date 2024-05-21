c152855bc410111865e80b4ac8db14b267ff03dd
Alex Kolodrybec <alex.kolodrybec@gmail.com>
2024-05-21 16:24:02 +0300

    Homework

--- Front-End-Pro/Homework_12.1/test.js
+++ Front-End-Pro/Homework_12.1/test.js
@@ -3,10 +3,11 @@ let url = '';
     function enterUrl() {
         url = prompt('Введіть посилання:');
     }
+    
     function redirectToUrl() {
         if (url) {
             window.location.href = url;
         } else {
-                alert('Спочатку введіть посилання.');
+            alert('Спочатку введіть посилання.');
         }
     }
