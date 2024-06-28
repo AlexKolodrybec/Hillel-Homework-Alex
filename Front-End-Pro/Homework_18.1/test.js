4f48c0e038aae1a8b11c223182542518d0bb3e3b
Alex Kolodrybec <alex.kolodrybec@gmail.com>
2024-06-28 15:12:22 +0300

    Homework

---
+++ Front-End-Pro/Homework_18.1/test.js
@@ -0,0 +1,18 @@
+let timeLeft = 85; 
+
+function updateTimer() {
+    const minutes = Math.floor(timeLeft / 60);
+    const seconds = timeLeft % 60;
+    document.getElementById('timer').textContent = 
+        `${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`;
+    
+    if (timeLeft > 0) {
+        timeLeft--;
+    } else {
+        clearInterval(timerInterval);
+    }
+}
+
+const timerInterval = setInterval(updateTimer, 1000);
+
+updateTimer();
